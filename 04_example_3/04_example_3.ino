4659b918370f5765125e81cc0f4c2dbffa7d6434
UH3135 <rabbitdy0101@kookmin.ac.kr>
2020-09-16 11:33:55 +0900

    창연공 2주차 과제 수정

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -3,6 +3,9 @@ unsigned int count, toggle;
 void setup() {
     pinMode(PIN_LED, OUTPUT);
     Serial.begin(115200); // Initialize serial port
+    While (!Serial){
+        ;
+    }
     Serial.println("Hello World!");
     count = toggle = 0;
     digitalWrite(PIN_LED, toggle); //turn off LED.
